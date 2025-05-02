d46cf0535c03180e05741808c2ef8b44e1813985
SSerov77 <sergo.sergej.05@yandex.ru>
2025-05-02 19:24:07 +0500

    refac + added pages order_failes + order_success

--- nyamrun/home/views.py
+++ nyamrun/home/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from places.models import Place, PlaceType
 
 def index(request):
-    # Получаем заведения по типам
     coffee_shops = Place.objects.filter(type=PlaceType.COFFEE_SHOP)
     bakeries = Place.objects.filter(type=PlaceType.BAKERY)
     cafes = Place.objects.filter(type=PlaceType.CAFE)
-
-    print(cafes)
 
     context = {
         'coffee_shops': coffee_shops,
