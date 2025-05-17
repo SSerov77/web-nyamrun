de208df7494c463338b3017c673f6e9a50a0d317
SSerov77 <sergo.sergej.05@yandex.ru>
2025-05-17 13:40:28 +0500

    refac

--- nyamrun/places/models.py
+++ nyamrun/places/models.py
@@ -56,11 +56,16 @@ class Place(models.Model):
         verbose_name="Изображение",
     )
     categories = models.ManyToManyField(
-        "catalog.Category", related_name="places", verbose_name="Категории товаров"
+        "catalog.Category",
+        related_name="places",
+        verbose_name="Категории товаров",
     )
     working_hours = models.CharField(max_length=255, verbose_name="Время работы")
     addresses = models.ManyToManyField(
-        Address, blank=True, related_name="places", verbose_name="Адреса"
+        Address,
+        blank=True,
+        related_name="places",
+        verbose_name="Адреса",
     )
     owner = models.ForeignKey(
         "users.CustomUser",
