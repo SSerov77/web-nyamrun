6692477597e31911443718ec0590523a1c33aee7
SSerov77 <sergo.sergej.05@yandex.ru>
2025-06-16 11:13:02 +0500

    Added postgres

--- nyamrun/nyamrun/settings.py
+++ nyamrun/nyamrun/settings.py
@@ -70,13 +70,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "nyamrun.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "nyamrun",
-        "USER": "postgres",
-        "PASSWORD": "admin",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('DB_NAME', 'nyamrun_db'),
+        'USER': getenv('DB_USER', 'nyamrun_user'),
+        'PASSWORD': getenv('DB_PASSWORD', 'nyamrun_pass'),
+        'HOST': getenv('DB_HOST', 'db'),
+        'PORT': getenv('DB_PORT', '5432'),
     }
 }
 
